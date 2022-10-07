04492871acf18afe87bdc26e2f19c902d607f4c6
Debanjan Poddar <debanjan2602@gmail.com>
2022-10-08 00:21:45 +0530

    Added note validation middleware using joi

--- middleware.js
+++ middleware.js
@@ -1,8 +1,21 @@
+const { noteSchema } = require("./schemas");
+const ExpressError = require("./utils/ExpressError");
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
         req.flash("error", "You must be logged in!");
         res.redirect("/login");
+    } else {
+        next();
+    }
+};
+
+module.exports.validateNote = (req, res, next) => {
+    const { error } = noteSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(", ");
+        next(new ExpressError(msg, 400));
     } else {
         next();
     }
