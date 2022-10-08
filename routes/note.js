ab750e1e043feb41845c100f975ac85ece39caad
Debanjan Poddar <debanjan2602@gmail.com>
2022-10-08 16:53:36 +0530

    Added GET and PUT routes for note editing

--- routes/note.js
+++ routes/note.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 
-const { isLoggedIn, validateNote } = require("../middleware");
+const { isLoggedIn, validateNote, isNoteOwner } = require("../middleware");
 const catchAsync = require("../utils/catchAsync");
 
 const notes = require("../controllers/notes");
@@ -12,5 +12,14 @@ router
     .post(isLoggedIn, validateNote, catchAsync(notes.postNote));
 
 router.get("/new", isLoggedIn, notes.renderNewNote);
+
+router.get(
+    "/:noteId/edit",
+    isLoggedIn,
+    isNoteOwner,
+    catchAsync(notes.renderEditForm)
+);
+
+router.put("/:noteId", isLoggedIn, isNoteOwner, catchAsync(notes.updateForm));
 
 module.exports = router;
