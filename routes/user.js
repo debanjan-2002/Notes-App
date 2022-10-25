6506f156d876d6ca823e5c8087227016d8587f34
Debanjan Poddar <debanjan2602@gmail.com>
2022-10-26 00:00:56 +0530

    Added verify-email GET route & added isUserVerified middleware in login route

--- routes/user.js
+++ routes/user.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
 
 const users = require("../controllers/users");
+const { isUserVerified } = require("../middleware");
 
 router
     .route("/login")
     .get(users.renderLogin)
     .post(
+        catchAsync(isUserVerified),
         passport.authenticate("local", {
             failureFlash: true,
             failureRedirect: "/login",
@@ -24,5 +26,7 @@ router
     .post(catchAsync(users.register));
 
 router.get("/logout", users.logout);
+
+router.get("/verify-email", catchAsync(users.verify));
 
 module.exports = router;
