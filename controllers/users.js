c3a51cb3926adad2fd98673cab3655abf35fb47c
Debanjan Poddar <debanjan2602@gmail.com>
2022-10-25 14:53:39 +0530

    sending email after registration

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User = require("../models/users");
+const transporter = require("../nodemailer");
 
 module.exports.renderRegister = (req, res) => {
     res.render("users/register");
@@ -16,6 +17,28 @@ module.exports.register = async (req, res, next) => {
             req.flash("success", `Welcome ${req.user.username}!`);
             res.redirect(`/${registeredUser._id}/notes`);
         });
+        // Email sending
+        try {
+            const mailDetails = {
+                from: `Notes app <${process.env.AUTH_EMAIL}>`,
+                to: email,
+                subject: `Welcome to Notes App!`,
+                text: "Thank You!",
+                html: `
+                <h3>Thank You for registering</h3>
+                <h3>Dear ${username},</h3>
+                <p>I am reaching out to thank you for registering to Notes App </p>
+                <p>Enjoy creating and storing notes :)</p>
+                <hr>
+                <p style="font-weight: bold;">Yours Faithfully</p>
+                <p style="font-weight: bold;">Debanjan Poddar (Notes App)</p> 
+                `,
+                replyTo: process.env.AUTH_EMAIL
+            };
+            await transporter.sendMail(mailDetails);
+        } catch (e) {
+            console.log(e.message);
+        }
     } catch (e) {
         req.flash("error", e.message);
         res.redirect("/register");
