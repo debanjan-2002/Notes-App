e4f85126afc3b199fd93d6644300e20bed2eb738
Debanjan Poddar <debanjan2602@gmail.com>
2022-10-26 17:22:28 +0530

    Added missing isOwner middleware in "note" routes

--- routes/note.js
+++ routes/note.js
@@ -1,28 +1,28 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 
-const { isLoggedIn, validateNote, isNoteOwner } = require("../middleware");
+const { isLoggedIn, validateNote, isOwner } = require("../middleware");
 const catchAsync = require("../utils/catchAsync");
 
 const notes = require("../controllers/notes");
 
 router
     .route("/")
-    .get(isLoggedIn, isNoteOwner, catchAsync(notes.getNotes))
-    .post(isLoggedIn, validateNote, catchAsync(notes.postNote));
+    .get(isLoggedIn, isOwner, catchAsync(notes.getNotes))
+    .post(isLoggedIn, isOwner, validateNote, catchAsync(notes.postNote));
 
-router.get("/new", isLoggedIn, notes.renderNewNote);
+router.get("/new", isLoggedIn, isOwner, notes.renderNewNote);
 
 router.get(
     "/:noteId/edit",
     isLoggedIn,
-    isNoteOwner,
+    isOwner,
     catchAsync(notes.renderEditForm)
 );
 
 router
     .route("/:noteId")
-    .put(isLoggedIn, isNoteOwner, catchAsync(notes.updateNote))
-    .delete(isLoggedIn, isNoteOwner, catchAsync(notes.deleteNote));
+    .put(isLoggedIn, isOwner, catchAsync(notes.updateNote))
+    .delete(isLoggedIn, isOwner, catchAsync(notes.deleteNote));
 
 module.exports = router;
