1ab959c9783b6c3edd1d5638e41094aa527ef664
Debanjan Poddar <debanjan2602@gmail.com>
2022-10-28 21:02:25 +0530

    Added logic for forget password & update password route

--- controllers/users.js
+++ controllers/users.js
@@ -24,7 +24,7 @@ module.exports.register = async (req, res, next) => {
             text: `
                 Thank you for registering on Notes App.
                 Dear ${username},
-                I am reaching out to thank you for registering to Notes App.
+                We are reaching out to thank you for registering to Notes App.
                 Please copy and paste the address below to verify your account.
                 http://${req.headers.host}/verify-email?token=${user.token}
                 Enjoy creating and storing notes :)
@@ -34,7 +34,7 @@ module.exports.register = async (req, res, next) => {
             html: `
                 <h3>Thank You for registering on Notes App.</h3>
                 <h3>Dear ${username},</h3>
-                <p>I am reaching out to thank you for registering to Notes App.</p>
+                <p>We are reaching out to thank you for registering to Notes App.</p>
                 <p>Please click the link below to verify your account.</p>
                 <a href="http://${req.headers.host}/verify-email?token=${user.token}">Verify your account</a>
                 <p>Enjoy creating and storing notes :)</p>
@@ -51,14 +51,14 @@ module.exports.register = async (req, res, next) => {
                 "success",
                 "Thank you for registering! Please check your email to verify your account"
             );
-            res.redirect("/register");
+            res.redirect("/login");
         } catch (e) {
             console.log(e.message);
             await User.findOneAndDelete({ username });
 
             req.flash(
                 "error",
-                "Something went wrong. Please contact us for assistance"
+                "Something went wrong. Please try registering again or contact us for assistance"
             );
             res.redirect("/register");
         }
@@ -68,7 +68,7 @@ module.exports.register = async (req, res, next) => {
     }
 };
 
-module.exports.verify = async (req, res) => {
+module.exports.verifyAccount = async (req, res) => {
     const { token } = req.query;
     try {
         const user = await User.findOne({ token });
@@ -82,7 +82,7 @@ module.exports.verify = async (req, res) => {
         user.token = null;
         user.isVerified = true;
         await user.save();
-        await req.login(user, err => {
+        req.login(user, err => {
             if (err) {
                 return next(err);
             }
@@ -152,5 +152,116 @@ module.exports.deleteAccount = async (req, res) => {
             "Something went wrong. Please contact us for assistance"
         );
         res.redirect(`/${userId}/notes`);
+    }
+};
+
+module.exports.renderForgetPassword = (req, res) => {
+    res.render("users/forget");
+};
+
+module.exports.forgetPassword = async (req, res) => {
+    const { registeredEmail } = req.body;
+    try {
+        const user = await User.findOne({ email: registeredEmail });
+        if (!user) {
+            req.flash("error", "No account found with this email");
+            return res.redirect("/forget-password");
+        }
+
+        user.token = uuid4();
+        await user.save();
+
+        const mailDetails = {
+            from: `Notes app <${process.env.AUTH_EMAIL}>`,
+            to: registeredEmail,
+            subject: `Reset Password - Notes App`,
+            text: `
+                Dear ${user.username},
+                We have received a request to reset the password of your account in Notes App.
+                Please copy and paste the address below to reset your password.
+                http://${req.headers.host}/update-password?token=${user.token}
+                Debanjan Poddar
+                Notes App
+            `,
+            html: `
+                <h3>Dear ${user.username},</h3>
+                <p>We have received a request to reset the password of your account in Notes App.</p>
+                <p>Please click the link below to reset your password.</p>
+                <a href="http://${req.headers.host}/update-password?token=${user.token}">Reset Password</a>
+                <hr>
+                <p style="font-weight: bold;">Debanjan Poddar</p> 
+                <p style="font-weight: bold;">Notes App</p>
+            `,
+            replyTo: process.env.AUTH_EMAIL
+        };
+        try {
+            await transporter.sendMail(mailDetails);
+            req.flash("success", "Check your email to reset your password");
+            res.redirect("/login");
+        } catch (e) {
+            console.log(e.message);
+            user.token = null;
+            await user.save();
+
+            req.flash(
+                "error",
+                "Something went wrong. Please try again or contact us for assistance"
+            );
+            res.redirect("/forget-password");
+        }
+    } catch (e) {
+        req.flash(
+            "error",
+            "Something went wrong. Please contact us for assistance"
+        );
+        res.redirect("/forget-password");
+    }
+};
+
+module.exports.renderUpdatePassword = async (req, res) => {
+    const { token } = req.query;
+    try {
+        const user = await User.findOne({ token });
+        if (!user) {
+            req.flash(
+                "error",
+                "Token is invalid! Please contact us for assistance"
+            );
+            return res.redirect("/login");
+        }
+        res.render("users/update", { user });
+    } catch (e) {
+        req.flash(
+            "error",
+            "Something went wrong. Please contact us for assistance"
+        );
+        res.redirect("/forget-password");
+    }
+};
+
+module.exports.updatePassword = async (req, res) => {
+    const { email, newPassword } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        await user.setPassword(newPassword);
+        user.token = null;
+        await user.save();
+
+        req.login(user, err => {
+            if (err) {
+                return next(err);
+            }
+            req.flash(
+                "success",
+                `Password reset successful! Welcome ${req.user.username}!`
+            );
+            res.redirect(`/${user._id}/notes`);
+        });
+    } catch (e) {
+        req.flash(
+            "error",
+            "Something went wrong. Please contact us for assistance"
+        );
+        res.redirect("/forget-password");
     }
 };
